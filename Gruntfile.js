250da42d77b6e820d401c7476f2a5563e794a15a
Jake Correa <jcorrea257@gmail.com>
2015-10-08 11:48:45 -0400

    Redirect from index page

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
         options: { pretty: true },
         files: [{
           expand: true,
-          cwd: 'src',
+          cwd: 'src/views',
           src: ['**/*.jade', '!**/_*.jade'],
           dest: 'www',
           ext: '.html'
